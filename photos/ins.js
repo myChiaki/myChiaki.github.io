402bc0e86172f40d1a70c4de18fc5abd61cd1f04
myChiaki <myChiaki@163.com>
2019-04-21 23:12:16 +0800

    change photos

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/myChiaki/myChiaki.github.io/master/min_photos/' + data.link[i] + '.min.jpg';
-          var src = 'https://raw.githubusercontent.com/myChiaki/myChiaki.github.io/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/myChiaki/blogPhotos/master/min_photos/' + data.link[i] + '.jpg';
+          var src = 'https://raw.githubusercontent.com/myChiaki/blogPhotos/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
